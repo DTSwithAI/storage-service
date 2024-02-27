7a83c99e97d477d69cf7036b1da0bf10c550bb6f
B1essed1 <mrshakh001@gmail.com>
2024-02-27 22:27:37 +0500

    start

---
+++ src/main/java/com/dts/storageserice/StorageSericeApplication.java
@@ -0,0 +1,13 @@
+package com.dts.storageserice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageSericeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageSericeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dts/storageserice/StorageSericeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dts.storageserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageSericeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
